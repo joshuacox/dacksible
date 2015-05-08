aa9927ecc55d50a42100dfdfff2c05eb8b54dd75
Josh Cox <josh@webhosting.coop>
2015-05-08 14:02:58 -0500

    newer ansible

--- packer/scripts/common/ansible.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-apt-get update
-apt-get install -y sudo ansible python-apt python-pycurl python-pip python-virtualenv
--- packer/scripts/ubuntu/ansible.sh
+++ packer/scripts/ubuntu/ansible.sh
@@ -1,6 +1,8 @@
 #/bin/sh
 apt-get update
-apt-get install software-properties-common
+apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get install -y sudo ansible python-apt python-pycurl python-pip python-virtualenv
+echo 'ansible version'
+ansible --version
