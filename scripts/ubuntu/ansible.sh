efdc03428ded1ddfacfabbab492564b0dd60b308
Josh Cox <josh@webhosting.coop>
2015-12-07 16:39:49 -0600

    curl wget vim

--- scripts/ubuntu/ansible.sh
+++ scripts/ubuntu/ansible.sh
@@ -3,6 +3,6 @@ apt-get update
 apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
-apt-get install -y sudo ansible python-apt python-pycurl python-pip python-virtualenv
+apt-get install -y sudo ansible python-apt python-pycurl python-pip python-virtualenv curl wget vim
 echo 'ansible version'
 ansible --version
