fede8ea3a6385b5eb71ce8193a20fc2fcbac48be
Josh Cox <josh@webhosting.coop>
2016-05-10 20:24:32 -0500

    scripts

--- scripts/ubuntu/cleanup.sh
+++ scripts/ubuntu/cleanup.sh
@@ -1,34 +1,42 @@
-#!/bin/bash -eux
+#!/bin/sh -eux
 
-# delete all linux headers
-dpkg --list | awk '{ print $2 }' | grep linux-headers | xargs apt-get -y purge
+# Delete all Linux headers
+dpkg --list \
+  | awk '{ print $2 }' \
+  | grep 'linux-headers' \
+  | xargs apt-get -y purge;
 
-# this removes specific linux kernels, such as
-# linux-image-3.11.0-15-generic but 
-# * keeps the current kernel
-# * does not touch the virtual packages, e.g.'linux-image-generic', etc.
-#
-dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v `uname -r` | xargs apt-get -y purge
+# Remove specific Linux kernels, such as linux-image-3.11.0-15-generic but
+# keeps the current kernel and does not touch the virtual packages,
+# e.g. 'linux-image-generic', etc.
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep 'linux-image-3.*-generic' \
+    | grep -v `uname -r` \
+    | xargs apt-get -y purge;
 
-# delete linux source
-dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
+# Delete Linux source
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep linux-source \
+    | xargs apt-get -y purge;
 
-# delete development packages
-dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
+# Delete development packages
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep -- '-dev$' \
+    | xargs apt-get -y purge;
 
-# delete compilers and other development tools
-apt-get -y purge cpp gcc g++
+# Delete X11 libraries
+apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6;
 
-# delete X11 libraries
-apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
+# Delete obsolete networking
+apt-get -y purge ppp pppconfig pppoeconf;
 
-# delete obsolete networking
-apt-get -y purge ppp pppconfig pppoeconf
+# Delete oddities
+apt-get -y purge popularity-contest;
 
-# delete oddities
-apt-get -y purge popularity-contest
+apt-get -y autoremove;
+apt-get -y clean;
 
-apt-get -y autoremove
-apt-get -y clean
-rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
-rm -f /tmp/chef*deb
+rm -f VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?;
--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -1,30 +1,34 @@
-#!/bin/bash -eux
+#!/bin/sh -eux
 
-UBUNTU_VERSION=`lsb_release -r | awk '{print $2}'`
-# on 12.04 work around bad cached lists
-if [[ "$UBUNTU_VERSION" == '12.04' ]]; then
-  apt-get clean
-  rm -rf /var/lib/apt/lists
+ubuntu_version="`lsb_release -r | awk '{print $2}'`";
+ubuntu_major_version="`echo $ubuntu_version | awk -F. '{print $1}'`";
+
+# Work around bad cached lists on Ubuntu 12.04
+if [ "$ubuntu_version" = "12.04" ]; then
+    apt-get clean;
+    rm -rf /var/lib/apt/lists;
 fi
 
+# Disable release-upgrades
+sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades;
+
 # Update the package list
-apt-get update
+apt-get -y update;
 
 # Upgrade all installed packages incl. kernel and kernel headers
-apt-get -y upgrade linux-server linux-headers-server
-
-# ensure the correct kernel headers are installed
-apt-get -y install linux-headers-$(uname -r)
+apt-get -y dist-upgrade --force-yes;
+reboot;
+sleep 60;
 
 # update package index on boot
-cat <<EOF > /etc/init/refresh-apt.conf
+cat <<EOF >/etc/init/refresh-apt.conf;
 description "update package index"
 start on networking
 task
 exec /usr/bin/apt-get update
 EOF
 
-# on 12.04 manage broken indexes on distro disc 12.04.5
-if [[ $UBUNTU_VERSION == '12.04' ]]; then
-  apt-get -y install libreadline-dev dpkg
+# Manage broken indexes on distro disc 12.04.5
+if [ "$ubuntu_version" = "12.04" ]; then
+    apt-get -y install libreadline-dev dpkg;
 fi
